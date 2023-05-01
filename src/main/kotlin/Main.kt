57391988be090651ca2182fa948ac1f9d7c0a837
Sonal <sonal.dhage26@gmail.com>
2023-05-01 14:11:57 +0200

    displaying current date and time

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,9 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.util.Date
+import java.text.SimpleDateFormat
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    val currentDate = Date()
+    val dateFormat = SimpleDateFormat("dd/MM/yyyy HH:mm:ss")
+    val formattedDate = dateFormat.format(currentDate)
+    println("Current date and time: $formattedDate")
 }
